26f94ca861aaaeec1fd2b5942c90fecc6ce98fd6
erickoficial69 <erickoficial69@gmail.com>
2019-11-19 16:23:15 +0000

    solucionando cagadas de git 3

--- src/rutas/dashboard/newOrder.js
+++ src/rutas/dashboard/newOrder.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React,{Fragment} from 'react'
 import Calculator from '../../components/calculator/calculator'
 import './NewOrder.css'
@@ -16,23 +15,4 @@ function NewOrder(props){
         </Fragment>
     )
 }
-=======
-import React,{Fragment} from 'react'
-import Calculator from '../../components/calculator/calculator'
-import './NewOrder.css'
-
-function NewOrder(props){
-    const {propietario} = props.match.params
-    return(
-        <Fragment>
-            <div className='container'>
-                <h1>Nueva Orden</h1>
-                <section className='contanerCalc Cartas CalculadoraOrden'>
-                    <Calculator propietario={propietario}/>
-                </section>
-            </div>
-        </Fragment>
-    )
-}
->>>>>>> 4db5092d6467c836177423a26bfe47beb4295217
 export default NewOrder
--- src/rutas/formMessages.js
+++ src/rutas/formMessages.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React,{Fragment} from 'react'
 import cerrarBtn from '../svg/cerrarBlanca.svg'
 import {sendMessage} from '../gets_apis/sockets'
@@ -59,66 +58,4 @@ const FormMessage = (props)=>{
         </Fragment>
     )
 }
-=======
-import React,{Fragment} from 'react'
-import cerrarBtn from '../svg/cerrarBlanca.svg'
-import {sendMessage} from '../gets_apis/sockets'
-const datosFecha = new Date()
-const fecha ={
-  
-    hora:datosFecha.toLocaleDateString()
-}
-const FormMessage = (props)=>{
-    const {setModalMessage, modalMessage, idRemitente,remitente,idDestinatario,destinatario} = props
-
-    const captureMessage= async e =>{
-        e.preventDefault()
-        const {idRemitente,remitente,idDestinatario,destinatario,titulo,mensaje} = e.target
-        const message={
-            idRemitente:idRemitente.value,
-            remitente:remitente.value,
-            idDestinatario:idDestinatario.value,
-            destinatario:destinatario.value,
-            titulo:titulo.value,
-            mensaje:mensaje.value,
-            fecha:fecha.hora,
-            mensajeStatus:'nuevo'
-        }
-        sendMessage(message,setModalMessage)
-    }
-
-    return(
-        <Fragment>
-            <div id='containerForm'>
-                <article className='ConfirmarOrden Cartas'>
-                <form onSubmit={captureMessage}>
-                    <div className='CampoFormulario'>
-                    <label>
-                        Titulo
-                    </label>
-                    <input type='text' name='titulo' placeholder='titulo del mensaje'/>
-                    </div>
-                    <div className='CampoFormulario'>
-                    <label>
-                        Mensaje
-                    </label>
-                    <textarea name='mensaje'>
-
-                    </textarea>
-                    </div>
-
-                        <input className='btnGreen BTN' type='submit' value='enviar' />
-
-                    <input type='hidden' name='idRemitente' value={idRemitente}/>
-                    <input type='hidden' name='remitente' value={remitente}/>
-                    <input type='hidden' name='idDestinatario' value={idDestinatario}/>
-                    <input type='hidden' name='destinatario' value={destinatario} />
-                </form>
-                <span onClick={()=>{setModalMessage(modalMessage ? false : true)}}className="BtnCerrar1"><img src={cerrarBtn} alt=""/></span>
-                </article>
-            </div>
-        </Fragment>
-    )
-}
->>>>>>> 4db5092d6467c836177423a26bfe47beb4295217
 export default FormMessage
