5deadd738bafa894930ee942a1c7703c34b2458f
erickoficial69 <erickoficial69@gmail.com>
2019-11-20 17:18:50 +0000

    modificando css pwa

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 const servers={
-   devServer:'https://backendvcoin.herokuapp.com', //'http://localhost:4000'
+   devServer:'https://backendvcoin.herokuapp.com', //'http://localhost:4000  https://4000-a8cef8df-d230-41a5-884f-1f3b7a95481e.ws-us02.gitpod.io'
    staticServer:'https://storagevcoin.ml'  //'http://localhost:80/storagevcoin'
 }
 
